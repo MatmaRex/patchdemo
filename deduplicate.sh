4b3e7527d1f300f5f8eda5e3edd40c2f3519c83c
Bartosz Dziewoński <matma.rex@gmail.com>
2020-01-04 22:20:15 +0100

    Add script to deduplicate source code files between wikis

---
+++ deduplicate.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Using hardlinks assumes that these files will never change,
+# as changing any of the deduplicated copies would affect all of them.
+
+# We can't use symlinks because PHP files using __FILE__, __DIR__ etc.
+# resolve them to the target file.
+
+# We could use reflinks but they only work on some filesystems, so
+# I'd have to figure out how to provision instances using XFS or something.
+# (Also, `rdfind` doesn't have an option for that, `rmlint` could be used.)
+
+rdfind -makehardlinks true -makeresultsfile false -checksum sha1 \
+[0-9a-f][0-9a-f][0-9a-f][0-9a-f][0-9a-f][0-9a-f][0-9a-f][0-9a-f][0-9a-f][0-9a-f][0-9a-f][0-9a-f][0-9a-f][0-9a-f][0-9a-f][0-9a-f][0-9a-f][0-9a-f][0-9a-f][0-9a-f][0-9a-f][0-9a-f][0-9a-f][0-9a-f][0-9a-f][0-9a-f][0-9a-f][0-9a-f][0-9a-f][0-9a-f][0-9a-f][0-9a-f]
--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 # dependencies of MediaWiki
 sudo apt-get install apache2 mysql-server php libapache2-mod-php php-mysql php-intl php-xml php-mbstring php-curl
 # dependencies of our system
-sudo apt-get install git composer
+sudo apt-get install git composer rdfind
 
 # create master copies of repositories
 sudo -u www-data mkdir repositories
@@ -10,6 +10,7 @@ while IFS=' ' read -r repo dir; do
 	sudo -u www-data git clone --no-checkout https://gerrit.wikimedia.org/r/$repo.git $repo
 done < ../repositories.txt
 cd ..
+
 
 # Composer wants a directory for itself (COMPOSER_HOME)
 sudo -u www-data mkdir composer
