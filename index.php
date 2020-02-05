0eef8c6d4c4bc33d8f1faec9cb546a0154929a69
Ed Sanders <ejsanders@gmail.com>
2020-02-05 11:33:25 -0800

    Add basic stylesheet for index.php

--- index.php
+++ index.php
@@ -3,86 +3,88 @@
 	<head>
 		<meta charset="utf-8">
 		<title>Patch demo</title>
+		<link rel="stylesheet" href="index.css">
 	</head>
 	<body>
+		<h1>Patch demo</h1>
 		<form action="new.php" method="POST">
-			<p>Start with version:</p>
-			<select name="branch">
+			<label>
+				<div>Start with version:</div>
+				<select name="branch">
+				<?php
+				require_once "includes.php";
+
+				$gitcmd = "git --git-dir=" . __DIR__ . "/repositories/mediawiki/core/.git";
+				// basically `git branch -r`, but without the silly parts
+				$branches = explode( "\n", shell_exec( "$gitcmd for-each-ref refs/remotes/origin/ --format='%(refname:short)'" ) );
+
+				$branches = array_filter( $branches, function ( $branch ) {
+					return preg_match( '/^origin\/(master|wmf|REL)/', $branch );
+				} );
+				natcasesort( $branches );
+
+				foreach ( $branches as $branch ) {
+					echo "<option>" . htmlspecialchars( $branch ) . "</option>\n";
+				}
+
+				?>
+				</select>
+			</label>
+			<label>
+				<div>Then, apply patches:</div>
+				<textarea name="patches" placeholder="Gerrit changeset number or Change-Id, one per line" rows="4" cols="50"></textarea>
+			</label>
+			<button type="submit">Create demo</button>
+		</form>
+		<table class="wikis">
+			<caption>Previously generated wikis</caption>
+			<tr><th>Name</th><th>Link</th><th>Time</th></tr>
 			<?php
-			require_once "includes.php";
-
-			$gitcmd = "git --git-dir=" . __DIR__ . "/repositories/mediawiki/core/.git";
-			// basically `git branch -r`, but without the silly parts
-			$branches = explode( "\n", shell_exec( "$gitcmd for-each-ref refs/remotes/origin/ --format='%(refname:short)'" ) );
-
-			$branches = array_filter( $branches, function ( $branch ) {
-				return preg_match( '/^origin\/(master|wmf|REL)/', $branch );
+			$dirs = array_filter( scandir( 'wikis' ), function ( $dir ) {
+				return substr( $dir, 0, 1 ) !== '.';
 			} );
-			natcasesort( $branches );
 
-			foreach ( $branches as $branch ) {
-				echo "<option>" . htmlspecialchars( $branch ) . "</option>\n";
+			$usecache = false;
+			$cache = @file_get_contents( 'wikicache.json' );
+			if ( $cache ) {
+				$wikis = json_decode( $cache, true );
+				$wikilist = array_keys( $wikis );
+				if ( sort( $wikilist ) === sort( $dirs ) ) {
+					$usecache = true;
+				}
 			}
 
-			?>
-			</select>
-
-			<p>Then, apply patches: <small>(Gerrit changeset number or Change-Id, one per line)</small></p>
-			<textarea name="patches"></textarea>
-
-			<br>
-			<button type="submit">Create demo</button>
-			<hr>
-			<table>
-				<caption>Previously generated wikis</caption>
-				<tr><th>Name</th><th>Link</th><th>Time</th></tr>
-				<?php
-				$dirs = array_filter( scandir( 'wikis' ), function ( $dir ) {
-					return substr( $dir, 0, 1 ) !== '.';
+			if ( !$usecache ) {
+				$wikis = [];
+				foreach ( $dirs as $dir ) {
+					if ( substr( $dir, 0, 1 ) !== '.' ) {
+						$title = '?';
+						$settings = @file_get_contents( 'wikis/' . $dir . '/w/LocalSettings.php' );
+						if ( $settings ) {
+							preg_match( '`wgSitename = "(.*)";`', $settings, $matches );
+							$title = $matches[ 1 ];
+						}
+						$wikis[ $dir ] = [
+							'mtime' => filemtime( 'wikis/' . $dir ),
+							'title' => $title
+						];
+					}
+				}
+				uksort( $wikis, function ( $a, $b ) use ( $wikis ) {
+					return $wikis[ $a ][ 'mtime' ] < $wikis[ $b ][ 'mtime' ];
 				} );
 
-				$usecache = false;
-				$cache = @file_get_contents( 'wikicache.json' );
-				if ( $cache ) {
-					$wikis = json_decode( $cache, true );
-					$wikilist = array_keys( $wikis );
-					if ( sort( $wikilist ) === sort( $dirs ) ) {
-						$usecache = true;
-					}
-				}
+				file_put_contents( 'wikicache.json', json_encode( $wikis ) );
+			}
 
-				if ( !$usecache ) {
-					$wikis = [];
-					foreach ( $dirs as $dir ) {
-						if ( substr( $dir, 0, 1 ) !== '.' ) {
-							$title = '?';
-							$settings = @file_get_contents( 'wikis/' . $dir . '/w/LocalSettings.php' );
-							if ( $settings ) {
-								preg_match( '`wgSitename = "(.*)";`', $settings, $matches );
-								$title = $matches[ 1 ];
-							}
-							$wikis[ $dir ] = [
-								'mtime' => filemtime( 'wikis/' . $dir ),
-								'title' => $title
-							];
-						}
-					}
-					uksort( $wikis, function ( $a, $b ) use ( $wikis ) {
-						return $wikis[ $a ][ 'mtime' ] < $wikis[ $b ][ 'mtime' ];
-					} );
-
-					file_put_contents( 'wikicache.json', json_encode( $wikis ) );
-				}
-
-				foreach ( $wikis as $wiki => $data ) {
-					echo '<tr>' .
-						'<td>' . $data[ 'title' ] . '</td>' .
-						'<td><a href="wikis/' . $wiki .'/w">' . $wiki .'</a></td>' .
-						'<td>' . date( 'c', $data[ 'mtime' ] ) . '</td>' .
-					'</tr>';
-				}
-				?>
-			</table>
-		</form>
+			foreach ( $wikis as $wiki => $data ) {
+				echo '<tr>' .
+					'<td>' . $data[ 'title' ] . '</td>' .
+					'<td><a href="wikis/' . $wiki .'/w">' . $wiki .'</a></td>' .
+					'<td>' . date( 'c', $data[ 'mtime' ] ) . '</td>' .
+				'</tr>';
+			}
+			?>
+		</table>
 	</body>
 </html>
