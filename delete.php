01efd3e1904c5e4e78a7ef5779733416d03af00d
Ed Sanders <ejsanders@gmail.com>
2020-03-10 12:52:11 +0000

    Allow deleting wikis from the web UI

---
+++ delete.php
@@ -0,0 +1,35 @@
+<?php
+
+require_once "includes.php";
+
+if ( !$config[ 'allowDelete' ] ) {
+	die( 'You must set <code>allowDelete</code> to <code>true</code> in <code>config.json</code>' );
+}
+
+$wiki = $_GET[ 'wiki' ];
+
+if ( !isset( $_POST['confirm' ] ) ) {
+	echo '<form method="POST">' .
+		'<p>Are you sure you want to delete this wiki: <a href="wikis/' . $wiki . '/w">' . $wiki . '</a>?</p>' .
+		'<p>This cannot be undone.</p>' .
+		'<button type="submit" name="confirm">Delete</button>' .
+	'</form>';
+	die();
+}
+
+ob_implicit_flush( true );
+
+$cmd = make_shell_command( [
+	'PATCHDEMO' => __DIR__,
+	'WIKI' => $wiki
+], __DIR__ . '/deletewiki.sh' );
+
+echo '<pre>';
+echo "$cmd\n";
+system( $cmd, $error );
+echo '</pre>';
+if ( $error ) {
+	die( "Could not delete." );
+}
+
+echo "Wiki deleted.";
--- includes.php
+++ includes.php
@@ -4,6 +4,13 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
+$config = json_decode( file_get_contents( 'config.default.json' ), true );
+$localConfig = get_if_file_exists( 'config.json' );
+
+if ( $localConfig ) {
+	$config = array_merge( $config, json_decode( $localConfig, true ) );
+}
+
 function make_shell_command( $env, $cmd ) {
 	$prefix = '';
 	foreach ($env as $key => $value) {
