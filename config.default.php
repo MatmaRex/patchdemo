89df23639095af7669901a706a157d24d2fc70d3
Ed Sanders <ejsanders@gmail.com>
2020-03-12 21:20:01 +0000

    OAuth: Allow users to delete their own wikis
    Allow admins to delete any wiki.

--- config.default.php
+++ config.default.php
@@ -1,9 +1,14 @@
 <?php
 $config = [
+	// Allow any user to delete wikis, e.g. on a private installation
 	'allowDelete' => false,
+	// OAuth config. When enabled only authenticated users can create
+	// wikis, and can delete their own wikis.
 	'oauth' => [
 		'url' => null,
 		'key' => null,
-		'secret' => null
+		'secret' => null,
+		// OAuth admins can delete any wiki
+		'admins' => []
 	]
 ];
