dcf1604011de5e1ea7fd41d73bb6fe90c1bb7388
Ed Sanders <ejsanders@gmail.com>
2020-05-28 19:31:04 +0100

    Add checkbox to show only the current user's wikis

--- index.js
+++ index.js
@@ -1,6 +1,21 @@
 ( function () {
-	var form = document.getElementById( 'new-form' );
+	var myWikis, wikisTable,
+		form = document.getElementById( 'new-form' );
+
 	form.addEventListener( 'submit', function () {
 		form.querySelector( 'button[type=submit]' ).disabled = true;
 	} );
+
+	if ( document.getElementsByClassName( 'myWikis' ).length ) {
+		myWikis = document.getElementsByClassName( 'myWikis' )[ 0 ];
+		wikisTable = document.getElementsByClassName( 'wikis' )[ 0 ];
+		myWikis.addEventListener( 'change', function () {
+			if ( myWikis.checked ) {
+				wikisTable.classList.add( 'hideOthers' );
+			} else {
+				wikisTable.classList.remove( 'hideOthers' );
+			}
+		} );
+	}
+
 }() );
