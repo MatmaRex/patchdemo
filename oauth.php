f8a932575e9d33d6f357121a58c8c16c20494252
Ed Sanders <ejsanders@gmail.com>
2020-05-28 22:03:00 +0100

    Fix layout of sign in page

--- oauth.php
+++ oauth.php
@@ -43,7 +43,7 @@ if ( $useOAuth ) {
 		$_SESSION['request_key'] = $token->key;
 		$_SESSION['request_secret'] = $token->secret;
 
-		echo "<a href='$authUrl'>Sign in with OAuth</a> to create and manage wikis.";
+		echo "</header><main><a href='$authUrl'>Sign in with OAuth</a> to create and manage wikis.";
 		include 'footer.html';
 		die();
 	}
