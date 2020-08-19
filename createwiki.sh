c28a408c69380b088e0781c4f9d26440b6c0a2c2
Bartosz Dziewoński <matma.rex@gmail.com>
2020-08-19 23:43:29 +0200

    Only add pages if GrowthExperiments is enabled

--- createwiki.sh
+++ createwiki.sh
@@ -48,10 +48,13 @@ cat $PATCHDEMO/LocalSettings.txt >> $PATCHDEMO/wikis/$NAME/w/LocalSettings.php
 # update Main_Page
 sleep 1 # Ensure edit appears after creation in history
 echo "$MAINPAGE" | php $PATCHDEMO/wikis/$NAME/w/maintenance/edit.php "Main_Page"
-for page in $(find $PATCHDEMO/pages -not -type d -printf '%P\n')
-do
-	cat "$PATCHDEMO/pages/$page" | php $PATCHDEMO/wikis/$NAME/w/maintenance/edit.php "$page"
-done
+
+if [ -d $PATCHDEMO/wikis/$NAME/w/extensions/GrowthExperiments ]; then
+	for page in $(find $PATCHDEMO/pages -not -type d -printf '%P\n')
+	do
+		cat "$PATCHDEMO/pages/$page" | php $PATCHDEMO/wikis/$NAME/w/maintenance/edit.php "$page"
+	done
+fi
 
 # copy logo
 cp $PATCHDEMO/images/logo.svg $PATCHDEMO/wikis/$NAME/w/
