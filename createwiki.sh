4a1a819220c0482f5897b5dd944b08b9a7b0f448
Ed Sanders <ejsanders@gmail.com>
2020-09-30 17:19:02 +0100

    Import pages unconditionally
    Previously this feature was only used by GrowthExperiments,
    but other extensions use it now (Citoid). It shouldn't take
    that long to just import all the pages anyway.

--- createwiki.sh
+++ createwiki.sh
@@ -53,12 +53,10 @@ cat $PATCHDEMO/LocalSettings.txt >> $PATCHDEMO/wikis/$NAME/w/LocalSettings.php
 sleep 1 # Ensure edit appears after creation in history
 echo "$MAINPAGE" | php $PATCHDEMO/wikis/$NAME/w/maintenance/edit.php "Main_Page"
 
-if [ -d $PATCHDEMO/wikis/$NAME/w/extensions/GrowthExperiments ]; then
-	for page in $(find $PATCHDEMO/pages -not -type d -printf '%P\n')
-	do
-		cat "$PATCHDEMO/pages/$page" | php $PATCHDEMO/wikis/$NAME/w/maintenance/edit.php "$page"
-	done
-fi
+for page in $(find $PATCHDEMO/pages -not -type d -printf '%P\n')
+do
+	cat "$PATCHDEMO/pages/$page" | php $PATCHDEMO/wikis/$NAME/w/maintenance/edit.php "$page"
+done
 
 # copy logo
 cp $PATCHDEMO/images/logo.svg $PATCHDEMO/wikis/$NAME/w/
