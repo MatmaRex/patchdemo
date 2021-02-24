bab978171018fe6d97232ceba42b63012f0709e3
Ed Sanders <ejsanders@gmail.com>
2021-02-24 16:13:30 +0000

    Split createwiki.sh into checkout & install
    Each of these two parts take about 15-20s, splitting
    them provides more granular progress when installing.

---
+++ new/install.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -ex
+
+# install
+cd $PATCHDEMO/wikis/$NAME/w
+php $PATCHDEMO/wikis/$NAME/w/maintenance/install.php \
+--dbname=patchdemo_$NAME \
+--dbuser=patchdemo \
+--dbpass=patchdemo \
+--confpath=$PATCHDEMO/wikis/$NAME/w \
+--server="$SERVER" \
+--scriptpath="$SERVERPATH/wikis/$NAME/w" \
+--with-extensions \
+--pass=patchdemo1 \
+"$WIKINAME" "Patch Demo"
+
+# apply our default settings
+cat $PATCHDEMO/LocalSettings.txt >> $PATCHDEMO/wikis/$NAME/w/LocalSettings.php
