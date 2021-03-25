18545c7f036faa8376d6e0b7e522bee84b445eb5
Ed Sanders <ejsanders@gmail.com>
2021-03-25 14:17:00 +0000

    PatchSelectWidget: Improve normalisation
    Always show patchset number in tag item. When deleting
    or editing, use the original user input.

--- api.php
+++ api.php
@@ -8,9 +8,12 @@ $patch = $_GET['patch'];
 
 preg_match( '/^(I[0-9a-f]+|(?<r>[0-9]+)(,(?<p>[0-9]+))?)$/', $patch, $matches );
 if ( isset( $matches['p'] ) ) {
-	$data = [ gerrit_query( "changes/{$matches['r']}/revisions/{$matches['p']}/commit" ) ];
+	$data = gerrit_query( "changes/{$matches['r']}/revisions/{$matches['p']}/commit" );
+	$data['r'] = $matches['r'];
+	$data['p'] = $matches['p'];
+	$data = [ $data ];
 } elseif ( isset( $matches['r'] ) ) {
-	$data = [ gerrit_query( "changes/{$matches['r']}" ) ];
+	$data = gerrit_query( "changes/?q=change:{$matches['r']}&o=LABELS&o=CURRENT_REVISION" );
 } else {
 	$data = gerrit_query( "changes/?q=change:$patch&o=LABELS&o=CURRENT_REVISION" );
 }
