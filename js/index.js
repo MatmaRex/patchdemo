0df4efdfe873937364dff78f55d73f0fd9f60d2b
Ed Sanders <ejsanders@gmail.com>
2021-03-28 13:47:28 +0100

    Add spacing above closed-wiki warning

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
 /* global OO, $ */
 ( function () {
-	var myWikis, closedWikis, branchSelect, form, submit, showClosed,
+	var myWikis, closedWikis, branchSelect, form, submit, showClosedButton,
 		patchesInput, patchesLayout, presetInput, reposField, reposInput, reposFieldLabel,
 		$wikisTable = $( '.wikis' );
 
@@ -43,9 +43,9 @@
 			closedWikis = OO.ui.infuse( $( '.closedWikis' ) );
 			closedWikis.on( 'change', updateTableClasses );
 
-			if ( $( '.showClosed' ).length ) {
-				showClosed = OO.ui.infuse( $( '.showClosed' ) );
-				showClosed.on( 'click', function () {
+			if ( $( '.showClosedButton' ).length ) {
+				showClosedButton = OO.ui.infuse( $( '.showClosedButton' ) );
+				showClosedButton.on( 'click', function () {
 					myWikis.setSelected( true );
 					closedWikis.setSelected( true );
 					updateTableClasses();
