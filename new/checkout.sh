2adf6e9d4d70bf4937cec3635db709beeec407c8
Bartosz Dziewoński <matma.rex@gmail.com>
2021-09-15 21:35:09 +0200

    Add Composer step for AbuseFilter
    Fixes #311

--- new/checkout.sh
+++ new/checkout.sh
@@ -27,6 +27,11 @@ if [ -d $PATCHDEMO/wikis/$NAME/w/extensions/VisualEditor ]; then
 	git submodule update --init --recursive
 fi
 
+if [ -d $PATCHDEMO/wikis/$NAME/w/extensions/AbuseFilter ]; then
+	cd $PATCHDEMO/wikis/$NAME/w/extensions/AbuseFilter
+	composer update --no-dev
+fi
+
 if [ -d $PATCHDEMO/wikis/$NAME/w/extensions/TemplateStyles ]; then
 	cd $PATCHDEMO/wikis/$NAME/w/extensions/TemplateStyles
 	composer update --no-dev
