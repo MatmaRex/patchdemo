84e2690c8eb5ab7c4e91647e30f500c7a02e0c30
Ed Sanders <ejsanders@gmail.com>
2021-10-27 18:45:38 +0100

    api.php: Check patch query string is present
    This shouldn't happen, but don't throw an uncaught error if it does.

--- api.php
+++ api.php
@@ -4,18 +4,22 @@ require_once "includes.php";
 
 header( 'Content-Type: application/json' );
 
-$patch = $_GET['patch'];
+$data = null;
 
-preg_match( '/^(I[0-9a-f]+|(?<r>[0-9]+)(,(?<p>[0-9]+))?)$/', $patch, $matches );
-if ( isset( $matches['p'] ) ) {
-	$data = gerrit_query( "changes/{$matches['r']}/revisions/{$matches['p']}/commit" );
-	$data['r'] = $matches['r'];
-	$data['p'] = $matches['p'];
-	$data = [ $data ];
-} elseif ( isset( $matches['r'] ) ) {
-	$data = gerrit_query( "changes/?q=change:{$matches['r']}&o=LABELS&o=CURRENT_REVISION" );
-} else {
-	$data = gerrit_query( "changes/?q=change:$patch&o=LABELS&o=CURRENT_REVISION" );
+if ( isset( $_GET['patch'] ) ) {
+	$patch = $_GET['patch'];
+
+	preg_match( '/^(I[0-9a-f]+|(?<r>[0-9]+)(,(?<p>[0-9]+))?)$/', $patch, $matches );
+	if ( isset( $matches['p'] ) ) {
+		$data = gerrit_query( "changes/{$matches['r']}/revisions/{$matches['p']}/commit" );
+		$data['r'] = $matches['r'];
+		$data['p'] = $matches['p'];
+		$data = [ $data ];
+	} elseif ( isset( $matches['r'] ) ) {
+		$data = gerrit_query( "changes/?q=change:{$matches['r']}&o=LABELS&o=CURRENT_REVISION" );
+	} else {
+		$data = gerrit_query( "changes/?q=change:$patch&o=LABELS&o=CURRENT_REVISION" );
+	}
 }
 
 echo json_encode( $data ?: [] );
