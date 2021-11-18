b051f51160c056e7f94430f2998e7894de2c4e39
Ed Sanders <ejsanders@gmail.com>
2021-11-18 15:23:38 +0000

    Checkout repos one at a time to report progress
    Requires splitting the `checkout.sh` into three parts.
    
    Move database creation to `install.sh`.

--- new/checkout.sh
+++ new/checkout.sh
@@ -1,38 +1,5 @@
 #!/bin/bash
 set -ex
 
-mkdir $PATCHDEMO/wikis/$NAME
-mkdir $PATCHDEMO/wikis/$NAME/w
-
-# check out files
-while IFS=' ' read -r repo dir; do
-	git --git-dir=$PATCHDEMO/repositories/$repo/.git worktree prune
-	git --git-dir=$PATCHDEMO/repositories/$repo/.git worktree add --detach $PATCHDEMO/wikis/$NAME/$dir $BRANCH
-done <<< "$REPOSITORIES"
-
-# make database
-mysql -u patchdemo --password=patchdemo -e "CREATE DATABASE patchdemo_$NAME";
-
-# fetch dependencies
-cd $PATCHDEMO/wikis/$NAME/w
-composer update --no-dev
-
-if [ -d $PATCHDEMO/wikis/$NAME/w/parsoid ]; then
-	cd $PATCHDEMO/wikis/$NAME/w/parsoid
-	composer update --no-dev
-fi
-
-if [ -d $PATCHDEMO/wikis/$NAME/w/extensions/VisualEditor ]; then
-	cd $PATCHDEMO/wikis/$NAME/w/extensions/VisualEditor
-	git submodule update --init --recursive
-fi
-
-if [ -d $PATCHDEMO/wikis/$NAME/w/extensions/AbuseFilter ]; then
-	cd $PATCHDEMO/wikis/$NAME/w/extensions/AbuseFilter
-	composer update --no-dev
-fi
-
-if [ -d $PATCHDEMO/wikis/$NAME/w/extensions/TemplateStyles ]; then
-	cd $PATCHDEMO/wikis/$NAME/w/extensions/TemplateStyles
-	composer update --no-dev
-fi
+git --git-dir=$PATCHDEMO/repositories/$REPO_SOURCE/.git worktree prune
+git --git-dir=$PATCHDEMO/repositories/$REPO_SOURCE/.git worktree add --detach $PATCHDEMO/wikis/$NAME/$REPO_TARGET $BRANCH
---
+++ new/precheckout.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+set -ex
+
+mkdir $PATCHDEMO/wikis/$NAME
+mkdir $PATCHDEMO/wikis/$NAME/w
