632f97e25f903dd9bdad6adab27df9365f0dcac3
Ed Sanders <ejsanders@gmail.com>
2021-12-07 15:36:52 +0000

    Run `git submodule update` on WikiLambda
    Part of #385

--- new/submodules.sh
+++ new/submodules.sh
@@ -5,3 +5,8 @@ if [ -d $PATCHDEMO/wikis/$NAME/w/extensions/VisualEditor ]; then
 	cd $PATCHDEMO/wikis/$NAME/w/extensions/VisualEditor
 	git submodule update --init --recursive
 fi
+
+if [ -d $PATCHDEMO/wikis/$NAME/w/extensions/WikiLambda ]; then
+	cd $PATCHDEMO/wikis/$NAME/w/extensions/WikiLambda
+	git submodule update --init --recursive
+fi
