2761e0cdac2bbe68c0ad0f4f172090d8d89b358c
MarcoAurelio <30000615+MarcoAurelioWM@users.noreply.github.com>
2022-01-26 11:55:40 +0100

    Grant globalblocking permissions to sysops
    Follow-up to https://github.com/MatmaRex/patchdemo/pull/419
    Assign to sysops all GlobalBlocking permissions for easier testing, and remove the steward user group as uneeded.

--- localsettings/extensions-GlobalBlocking.php
+++ localsettings/extensions-GlobalBlocking.php
@@ -1,3 +1,9 @@
 <?php
 
 $wgGlobalBlockingDatabase = $wgDBname;
+
+$wgGroupPermissions['sysop']['globalblock'] = true;
+$wgGroupPermissions['sysop']['globalblock-whitelist'] = true;
+$wgGroupPermissions['sysop']['globalblock-exempt'] = true;
+
+unset( $wgGroupPermissions['steward'] );
