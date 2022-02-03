881ab0547e4d85a96b213cc35930180e724d3d47
Ed Sanders <ejsanders@gmail.com>
2022-02-03 15:56:47 +0000

    Fix table section header positioning on mobile

--- index.php
+++ index.php
@@ -347,7 +347,7 @@ if ( $closedWikis ) {
 }
 
 echo '<table class="wikis">' .
-	'<tr>' .
+	'<tr class="headerRow">' .
 		'<th>Wiki</th>' .
 		'<th>Patches<br /><em>✓=Merged ✗=Abandoned 🛇=<abbr title="Open patches marked with \'DNM\' or \'DO NOT MERGE\'">Do not merge</abbr></em></th>' .
 		'<th>Linked tasks<br /><em>✓=Resolved ✗=Declined/Invalid</em></th>' .
