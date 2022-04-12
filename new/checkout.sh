149fe20fb5008908d8daa3dc995435cd410b8aa3
Bartosz Dziewoński <matma.rex@gmail.com>
2022-04-12 16:54:34 +0200

    Fix `git worktree` creating invalid refs
    Fixes #446

--- new/checkout.sh
+++ new/checkout.sh
@@ -2,4 +2,7 @@
 set -ex
 
 git --git-dir=$PATCHDEMO/repositories/$REPO_SOURCE/.git worktree prune
-git --git-dir=$PATCHDEMO/repositories/$REPO_SOURCE/.git worktree add --detach $PATCHDEMO/wikis/$NAME/$REPO_TARGET $BRANCH
+# Canonicalize the path using `readlink -f` to avoid a bug where `git worktree`
+# creates invalid refs when the path ends with '/.' (#446)
+git --git-dir=$PATCHDEMO/repositories/$REPO_SOURCE/.git worktree add \
+	--detach $(readlink -f $PATCHDEMO/wikis/$NAME/$REPO_TARGET) $BRANCH
