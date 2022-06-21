7cd6e815c2ed773fee74b4adecf89c736638e314
MarcoAurelio <30000615+MarcoAurelioWM@users.noreply.github.com>
2022-06-21 11:52:24 +0200

    Create extensions-IPInfo.php

---
+++ localsettings/extensions-IPInfo.php
@@ -0,0 +1,9 @@
+<? php
+
+$wgGroupPermissions['user']['ipinfo'] = true;
+$wgGroupPermissions['user']['ipinfo-view-basic'] = true;
+$wgGroupPermissions['sysop']['ipinfo-view-full'] = true;
+$wgGroupPermissions['sysop']['ipinfo-view-log'] = true;
+
+$wgAddGroups['sysop']['no-ipinfo'] = true;
+$wgRemoveGroups ['sysop']['no-ipinfo'] = true;
