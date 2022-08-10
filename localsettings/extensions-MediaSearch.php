1d63b2d48c4fa1b80f379c1f0fa532bf17998383
Ed Sanders <ejsanders@gmail.com>
2022-08-10 13:55:16 +0100

    Enable MediaSearch extension
    Not enabled by default because this is only installed on Commons.
    
    Configure to use external API results from Commons and WikiData,
    per on-wiki instructions.
    
    Fixes #487

---
+++ localsettings/extensions-MediaSearch.php
@@ -0,0 +1,7 @@
+<?php
+
+// External search results API
+$wgMediaSearchExternalSearchUri = 'https://commons.wikimedia.org/w/api.php';
+
+// External entity search base URI (for autocomplete suggestions)
+$wgMediaSearchExternalEntitySearchBaseUri = 'https://www.wikidata.org/w/api.php';
