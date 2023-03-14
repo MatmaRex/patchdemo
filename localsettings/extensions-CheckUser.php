aa2b9efe0174484996db325afd02b02eb91dfe52
Thalia <thalia.e.chan@googlemail.com>
2023-03-14 18:28:15 +0000

    Assign checkuser-temporary-account to checkuser group
    This allows testing IP Masking workflows on Patch Demo.

--- localsettings/extensions-CheckUser.php
+++ localsettings/extensions-CheckUser.php
@@ -3,3 +3,4 @@
 $wgGroupPermissions['checkuser'] = [];
 $wgGroupPermissions['checkuser']['checkuser'] = true;
 $wgGroupPermissions['checkuser']['checkuser-log'] = true;
+$wgGroupPermissions['checkuser']['checkuser-temporary-account'] = true;
