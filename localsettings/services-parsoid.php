9a6fcfd6f70e4de2be267107c8a34366c63afa35
Bartosz Dziewoński <matma.rex@gmail.com>
2023-03-24 01:20:27 +0100

    Fix developer setup of Parsoid after changes in MediaWiki
    Fixes #544

--- localsettings/services-parsoid.php
+++ localsettings/services-parsoid.php
@@ -4,9 +4,16 @@
 $wgEnableRestAPI = true;
 if ( file_exists( 'parsoid/extension.json' ) ) {
 	// Ensure we load the right Parsoid (not via Composer)
-	AutoLoader::$psr4Namespaces += [
-		'Wikimedia\\Parsoid\\' => 'parsoid/src',
-	];
+	if ( method_exists( AutoLoader::class, 'registerNamespaces' ) ) {
+		// https://phabricator.wikimedia.org/T332930
+		AutoLoader::registerNamespaces( [
+			'Wikimedia\\Parsoid\\' => 'parsoid/src',
+		] );
+	} else {
+		AutoLoader::$psr4Namespaces += [
+			'Wikimedia\\Parsoid\\' => 'parsoid/src',
+		];
+	}
 	wfLoadExtension( 'Parsoid', 'parsoid/extension.json' );
 }
 $wgParsoidSettings = [
