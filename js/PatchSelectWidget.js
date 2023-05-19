199d56bf3d346c85fb147394026415d6c083d75d
Bartosz Dziewoński <matma.rex@gmail.com>
2023-05-19 13:55:33 +0300

    Use ES6 classes for custom OOUI widgets
    Just for a lark.
    
    Do not use ES6 static properties though, as they're only introduced in
    ES2020, and also apparently everyone hates them.

--- js/PatchSelectWidget.js
+++ js/PatchSelectWidget.js
@@ -12,209 +12,209 @@
 	}
 }() );
 
-window.PatchSelectWidget = function PatchSelectWidget( config ) {
-	this.$formInput = $( '<input>' ).attr( {
-		type: 'hidden',
-		name: config.name
-	} );
+window.PatchSelectWidget = class PatchSelectWidget extends OO.ui.TagMultiselectWidget {
+	constructor( config ) {
+		super( $.extend( {
+			allowArbitrary: true,
+			allowDisplayInvalidTags: true,
+			selected: config.value && config.value.split( '\n' )
+		}, config ) );
 
-	// Parent constructor
-	window.PatchSelectWidget.super.call( this, $.extend( {
-		allowArbitrary: true,
-		allowDisplayInvalidTags: true,
-		selected: config.value && config.value.split( '\n' )
-	}, config ) );
+		this.$formInput = $( '<input>' ).attr( {
+			type: 'hidden',
+			name: config.name
+		} );
 
-	// Assume that a whole patch number was pasted
-	// TODO: Upstream?
-	this.input.$input.on( 'paste', () => {
-		setTimeout( this.doInputEnter.bind( this ) );
-	} );
+		// Assume that a whole patch number was pasted
+		// TODO: Upstream?
+		this.input.$input.on( 'paste', () => {
+			setTimeout( this.doInputEnter.bind( this ) );
+		} );
 
-	this.$element
-		.append( this.$formInput )
-		.addClass( 'PatchSelectWidget' );
-};
-
-OO.inheritClass( window.PatchSelectWidget, OO.ui.TagMultiselectWidget );
-
-window.PatchSelectWidget.static.patchCache = {};
-
-window.PatchSelectWidget.prototype.getTagInfoFromInput = function ( value ) {
-	value = value || this.input.getValue();
-
-	const gerritUrlPattern = new RegExp( pd.config.gerritUrl + '.*?/([0-9]+(?:/[0-9]+)?)/?$' );
-
-	value = value.trim();
-
-	const matches = value.match( gerritUrlPattern );
-	if ( matches ) {
-		value = matches[ 1 ].replace( '/', ',' );
+		this.$element
+			.append( this.$formInput )
+			.addClass( 'PatchSelectWidget' );
 	}
 
-	return value ?
-		{ data: value, label: value } :
-		null;
-};
+	getTagInfoFromInput( value ) {
+		value = value || this.input.getValue();
 
-window.PatchSelectWidget.prototype.addTagFromInput = function () {
-	// Handle multi line inputs, e.g. from paste
-	// TODO: Upstream?
-	const tagInfos = this.input.getValue().trim().split( /[ \n]/ )
-		.map( this.getTagInfoFromInput.bind( this ) );
+		const gerritUrlPattern = new RegExp( pd.config.gerritUrl + '.*?/([0-9]+(?:/[0-9]+)?)/?$' );
 
-	tagInfos.forEach( ( tagInfo ) => {
-		if ( tagInfo && this.addTag( tagInfo.data, tagInfo.label ) ) {
-			this.clearInput();
-		}
-	} );
-};
+		value = value.trim();
 
-window.PatchSelectWidget.prototype.onInputKeyPress = function ( e ) {
-	if ( !this.isDisabled() ) {
-		let stopOrContinue;
-		if ( e.which === OO.ui.Keys.SPACE ) {
-			const withMetaKey = e.metaKey || e.ctrlKey;
-			stopOrContinue = this.doInputEnter( e, withMetaKey );
+		const matches = value.match( gerritUrlPattern );
+		if ( matches ) {
+			value = matches[ 1 ].replace( '/', ',' );
 		}
 
-		// Make sure the input gets resized.
-		this.updateInputSize();
-
-		if ( stopOrContinue !== undefined ) {
-			return stopOrContinue;
-		}
+		return value ?
+			{ data: value, label: value } :
+			null;
 	}
 
-	return window.PatchSelectWidget.super.prototype.onInputKeyPress.apply( this, arguments );
-};
+	addTagFromInput() {
+		// Handle multi line inputs, e.g. from paste
+		// TODO: Upstream?
+		const tagInfos = this.input.getValue().trim().split( /[ \n]/ )
+			.map( this.getTagInfoFromInput.bind( this ) );
 
-window.PatchSelectWidget.prototype.createTagItemWidget = function () {
-	const patchCache = this.constructor.static.patchCache,
-		// eslint-disable-next-line max-len
-		item = window.PatchSelectWidget.super.prototype.createTagItemWidget.apply( this, arguments ),
-		patch = item.getData().trim();
-
-	function linkClick( e ) {
-		e.stopPropagation();
+		tagInfos.forEach( ( tagInfo ) => {
+			if ( tagInfo && this.addTag( tagInfo.data, tagInfo.label ) ) {
+				this.clearInput();
+			}
+		} );
 	}
 
-	item.setLabel( patch + ': …' );
-	item.setData( { input: patch } );
+	onInputKeyPress( e ) {
+		if ( !this.isDisabled() ) {
+			let stopOrContinue;
+			if ( e.which === OO.ui.Keys.SPACE ) {
+				const withMetaKey = e.metaKey || e.ctrlKey;
+				stopOrContinue = this.doInputEnter( e, withMetaKey );
+			}
 
-	patchCache[ patch ] = patchCache[ patch ] || (
-		patch.match( /^(I[0-9a-f]+|[0-9]+(,[0-9]+)?)$/ ) ?
-			$.get( 'api.php', { action: 'patchmeta', patch: patch } ) :
-			$.Deferred().reject( 'Invalid patch number' ).promise()
-	);
-	patchCache[ patch ].then( ( response ) => {
-		const data = { input: patch };
-		if ( !response.length ) {
-			return $.Deferred().reject( 'Could not find patch' ).promise();
-		} else if ( response.length > 1 ) {
-			return $.Deferred().reject( 'Ambiguous query' ).promise();
-		}
-		data.r = response[ 0 ].r ||
-			// eslint-disable-next-line no-underscore-dangle
-			( patch.indexOf( 'I' ) === 0 ? response[ 0 ]._number : patch );
-		data.p = response[ 0 ].p ||
-			// eslint-disable-next-line no-underscore-dangle
-			response[ 0 ].revisions[ response[ 0 ].current_revision ]._number;
-		data.linkedTasks = response[ 0 ].linkedTasks;
+			// Make sure the input gets resized.
+			this.updateInputSize();
 
-		item.setFlags( [] );
-		item.setData( data );
-		item.setLabel(
-			$( '<span>' ).append(
-				document.createTextNode( data.r + ',' + data.p + ': ' ),
-				$( '<a>' )
-					.attr( {
-						target: '_blank',
-						href: pd.config.gerritUrl + '/r/' + data.r
-					} )
-					.text( response[ 0 ].subject )
-					.on( 'click', linkClick )
-			)
-		);
-		// Update input after ID normalization
-		this.onChangeTags();
-		this.updateInputSize();
-	} ).then( null, ( err ) => {
-		item.setLabel( item.getData().input + ': ' + err );
-		item.toggleValid( false );
-		this.updateInputSize();
-	} );
-
-	return item;
-};
-
-window.PatchSelectWidget.prototype.onChangeTags = function () {
-	window.PatchSelectWidget.super.prototype.onChangeTags.apply( this, arguments );
-
-	this.$formInput.val(
-		// Join items with a pipe as the hidden input is single line
-		this.items.map( ( item ) => {
-			const data = item.getData();
-			return data.r ?
-				// 'latest' means the user didn't specify a patchset, and so wants the latest one.
-				( data.latest ? data.r : data.r + ',' + data.p ) :
-				data.input;
-		} ).join( '|' )
-	);
-
-	const linkedTasks = {};
-	this.items.forEach( ( item ) => {
-		const data = item.getData();
-		if ( data.linkedTasks ) {
-			data.linkedTasks.forEach( ( task ) => {
-				linkedTasks[ task ] = true;
-			} );
-		}
-	} );
-
-	this.emit( 'linkedTasks', Object.keys( linkedTasks ) );
-
-	const patchKey = this.items.map( ( item ) => {
-		const data = item.getData();
-		return data.r + ',' + data.p;
-	} ).sort().join( '|' );
-	this.emit( 'matchWikis', pd.patchIndex[ patchKey ] );
-};
-
-// Like the parent method, but works with outline, and uses getData instead of getLabel
-window.PatchSelectWidget.prototype.doInputBackspace = function ( e, withMetaKey ) {
-	if (
-		this.inputPosition === 'inline' &&
-		this.input.getValue() === '' &&
-		!this.isEmpty()
-	) {
-		// Delete the last item
-		const items = this.getItems();
-		const item = items[ items.length - 1 ];
-
-		if ( !item.isDisabled() && !item.isFixed() ) {
-			this.removeItems( [ item ] );
-			// If Ctrl/Cmd was pressed, delete item entirely.
-			// Otherwise put it into the text field for editing.
-			if ( !withMetaKey ) {
-				this.input.setValue( item.getData().input );
+			if ( stopOrContinue !== undefined ) {
+				return stopOrContinue;
 			}
 		}
 
-		return false;
+		return super.onInputKeyPress( ...arguments );
+	}
+
+	createTagItemWidget() {
+		const patchCache = this.constructor.static.patchCache,
+			item = super.createTagItemWidget( ...arguments ),
+			patch = item.getData().trim();
+
+		function linkClick( e ) {
+			e.stopPropagation();
+		}
+
+		item.setLabel( patch + ': …' );
+		item.setData( { input: patch } );
+
+		patchCache[ patch ] = patchCache[ patch ] || (
+			patch.match( /^(I[0-9a-f]+|[0-9]+(,[0-9]+)?)$/ ) ?
+				$.get( 'api.php', { action: 'patchmeta', patch: patch } ) :
+				$.Deferred().reject( 'Invalid patch number' ).promise()
+		);
+		patchCache[ patch ].then( ( response ) => {
+			const data = { input: patch };
+			if ( !response.length ) {
+				return $.Deferred().reject( 'Could not find patch' ).promise();
+			} else if ( response.length > 1 ) {
+				return $.Deferred().reject( 'Ambiguous query' ).promise();
+			}
+			data.r = response[ 0 ].r ||
+				// eslint-disable-next-line no-underscore-dangle
+				( patch.indexOf( 'I' ) === 0 ? response[ 0 ]._number : patch );
+			data.p = response[ 0 ].p ||
+				// eslint-disable-next-line no-underscore-dangle
+				response[ 0 ].revisions[ response[ 0 ].current_revision ]._number;
+			data.linkedTasks = response[ 0 ].linkedTasks;
+
+			item.setFlags( [] );
+			item.setData( data );
+			item.setLabel(
+				$( '<span>' ).append(
+					document.createTextNode( data.r + ',' + data.p + ': ' ),
+					$( '<a>' )
+						.attr( {
+							target: '_blank',
+							href: pd.config.gerritUrl + '/r/' + data.r
+						} )
+						.text( response[ 0 ].subject )
+						.on( 'click', linkClick )
+				)
+			);
+			// Update input after ID normalization
+			this.onChangeTags();
+			this.updateInputSize();
+		} ).then( null, ( err ) => {
+			item.setLabel( item.getData().input + ': ' + err );
+			item.toggleValid( false );
+			this.updateInputSize();
+		} );
+
+		return item;
+	}
+
+	onChangeTags() {
+		super.onChangeTags( ...arguments );
+
+		this.$formInput.val(
+			// Join items with a pipe as the hidden input is single line
+			this.items.map( ( item ) => {
+				const data = item.getData();
+				return data.r ?
+					// 'latest' means the user didn't specify a patchset, and so wants the latest one.
+					( data.latest ? data.r : data.r + ',' + data.p ) :
+					data.input;
+			} ).join( '|' )
+		);
+
+		const linkedTasks = {};
+		this.items.forEach( ( item ) => {
+			const data = item.getData();
+			if ( data.linkedTasks ) {
+				data.linkedTasks.forEach( ( task ) => {
+					linkedTasks[ task ] = true;
+				} );
+			}
+		} );
+
+		this.emit( 'linkedTasks', Object.keys( linkedTasks ) );
+
+		const patchKey = this.items.map( ( item ) => {
+			const data = item.getData();
+			return data.r + ',' + data.p;
+		} ).sort().join( '|' );
+		this.emit( 'matchWikis', pd.patchIndex[ patchKey ] );
+	}
+
+	// Like the parent method, but works with outline, and uses getData instead of getLabel
+	doInputBackspace( e, withMetaKey ) {
+		if (
+			this.inputPosition === 'inline' &&
+			this.input.getValue() === '' &&
+			!this.isEmpty()
+		) {
+			// Delete the last item
+			const items = this.getItems();
+			const item = items[ items.length - 1 ];
+
+			if ( !item.isDisabled() && !item.isFixed() ) {
+				this.removeItems( [ item ] );
+				// If Ctrl/Cmd was pressed, delete item entirely.
+				// Otherwise put it into the text field for editing.
+				if ( !withMetaKey ) {
+					this.input.setValue( item.getData().input );
+				}
+			}
+
+			return false;
+		}
+	}
+
+	onTagSelect( item ) {
+		if ( this.hasInput && this.allowEditTags && !item.isFixed() ) {
+			if ( this.input.getValue() ) {
+				this.addTagFromInput();
+			}
+			// 1. Get the label of the tag into the input
+			this.input.setValue( item.getData().input );
+			// 2. Remove the tag
+			this.removeItems( [ item ] );
+			// 3. Focus the input
+			this.focus();
+		}
 	}
 };
 
-window.PatchSelectWidget.prototype.onTagSelect = function ( item ) {
-	if ( this.hasInput && this.allowEditTags && !item.isFixed() ) {
-		if ( this.input.getValue() ) {
-			this.addTagFromInput();
-		}
-		// 1. Get the label of the tag into the input
-		this.input.setValue( item.getData().input );
-		// 2. Remove the tag
-		this.removeItems( [ item ] );
-		// 3. Focus the input
-		this.focus();
-	}
-};
+OO.initClass( window.PatchSelectWidget );
+
+window.PatchSelectWidget.static.patchCache = {};
