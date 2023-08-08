3351a3c39497e709efe3ed5bdf40b3e1f7f2309b
Bartosz Dziewoński <matma.rex@gmail.com>
2023-08-08 18:11:35 +0200

    Match IP Masking configuration to Wikimedia production

--- localsettings/feature-tempuser.php
+++ localsettings/feature-tempuser.php
@@ -1,3 +1,7 @@
 <?php
 
 $wgAutoCreateTempUser['enabled'] = true;
+
+// editing is enabled only for temp accounts
+$wgGroupPermissions['*']['edit'] = false;
+$wgGroupPermissions['temp']['edit'] = true;
